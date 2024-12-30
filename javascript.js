66e63cbaee63dabe63562bdbf6fe8e8cc13d1e3a
Alicia Kee <155275698+alicia-kee@users.noreply.github.com>
2024-12-29 19:50:15 -0600

    Made 2 new images that shows pictures on what player and computer chooses

--- javascript.js
+++ javascript.js
@@ -25,37 +25,52 @@ function playGame(){
     let humanScore = 0;
     let computerScore = 0;
 
-    function playRound(compChoice, humanChoice, resultContainer, scoreContainer, winnerContainer){
+    function playRound(compChoice, humanChoice){
         if(compChoice==="rock"){
+            compChoiceImg.src = "./images/rock.jpg";
+
             if(humanChoice==="rock"){
+                playerChoiceImg.src = "./images/rock.jpg";
                 resultContainer.textContent = "Tie! You both chose Rock.";
             }else if(humanChoice==="paper"){
+                playerChoiceImg.src = "./images/paper.jpg";
                 resultContainer.textContent = "You win! Paper beats Rock.";
                 humanScore++;
             }else if(humanChoice==="scissors"){
+                playerChoiceImg.src = "./images/scissors.jpg";
                 resultContainer.textContent = "You lose! Rock beats Scissors.";
                 computerScore++;
             }
     
         }else if(compChoice==="paper"){
+            compChoiceImg.src = "./images/paper.jpg";
+
             if(humanChoice==="rock"){
+                playerChoiceImg.src = "./images/rock.jpg";
                 resultContainer.textContent = "You lose! Paper beats Rock.";
                 computerScore++;
             }else if(humanChoice==="paper"){
+                playerChoiceImg.src = "./images/paper.jpg";
                 resultContainer.textContent = "Tie! You both chose Paper.";
             }else if(humanChoice==="scissors"){
+                playerChoiceImg.src = "./images/scissors.jpg";
                 resultContainer.textContent = "You win! Scissors beats Paper.";
                 humanScore++;
             }
     
         }else if(compChoice==="scissors"){
+            compChoiceImg.src = "./images/scissors.jpg";
+
             if(humanChoice==="rock"){
+                playerChoiceImg.src = "./images/rock.jpg";
                 resultContainer.textContent = "You win! Rock beats Scissors.";
                 humanScore++;
             }else if(humanChoice==="paper"){
+                playerChoiceImg.src = "./images/paper.jpg";
                 resultContainer.textContent = "You lose! Scissors beats Paper.";
                 computerScore++;
             }else if(humanChoice==="scissors"){
+                playerChoiceImg.src = "./images/scissors.jpg";
                 resultContainer.textContent = "Tie! You both chose Scissors.";
             }
         }
@@ -104,21 +119,35 @@ function playGame(){
     // }
     // displayWinner();
 
-    //NEW FOR UI:
-    let rockBtn = document.querySelector('#rock');
-    let paperBtn = document.querySelector('#paper');
-    let scissorsBtn = document.querySelector('#scissors');
-    //don't need getHumanChoice() anymore?
-    let computerSelection = getComputerChoice();
-    const resultContainer = document.querySelector("#result");
-    const scoreContainer = document.querySelector("#scores");
-    const winnerContainer = document.querySelector("#winner");
+    // rockBtn.addEventListener('click', () => {playRound (computerSelection, "rock", resultContainer, scoreContainer, winnerContainer);});
+    // paperBtn.addEventListener('click', () => {playRound (computerSelection, "paper", resultContainer, scoreContainer, winnerContainer);});
+    // scissorsBtn.addEventListener('click', () => {playRound (computerSelection, "scissors", resultContainer, scoreContainer, winnerContainer);});
 
-    rockBtn.addEventListener('click', () => {playRound (computerSelection, "rock", resultContainer, scoreContainer, winnerContainer);});
-    paperBtn.addEventListener('click', () => {playRound (computerSelection, "paper", resultContainer, scoreContainer, winnerContainer);});
-    scissorsBtn.addEventListener('click', () => {playRound (computerSelection, "scissors", resultContainer, scoreContainer, winnerContainer);});
-
+    rockBtn.addEventListener('click', () => {playRound (getComputerChoice(), "rock");});
+    paperBtn.addEventListener('click', () => {playRound (getComputerChoice(), "paper");});
+    scissorsBtn.addEventListener('click', () => {playRound (getComputerChoice(), "scissors");});
 } //end playGame function
+
+//NEW FOR UI:
+let rockBtn = document.querySelector('#rock');
+let paperBtn = document.querySelector('#paper');
+let scissorsBtn = document.querySelector('#scissors');
+// let computerSelection = getComputerChoice();
+
+const resultContainer = document.querySelector("#result");
+const scoreContainer = document.querySelector("#scores");
+const winnerContainer = document.querySelector("#winner");
+
+const playerChoice = document.querySelector(".player-choice");
+const compChoice= document.querySelector(".comp-choice");
+const playerChoiceImg = document.createElement("img");
+playerChoice.appendChild(playerChoiceImg);
+playerChoiceImg.width = "160";
+playerChoiceImg.height="160";
+const compChoiceImg = document.createElement("img");
+compChoice.appendChild(compChoiceImg);
+compChoiceImg.width = "160";
+compChoiceImg.height = "160";
 
 playGame();
 
